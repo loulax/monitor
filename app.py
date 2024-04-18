c84ee8dae52722ae291c83049046dff044c8c324
loulax <gestion-loulax@proton.me>
2024-04-18 22:55:29 +0200

    opti loader + new template

--- app.py
+++ app.py
@@ -19,47 +19,33 @@ class Raspberry:
         else:
             return False
 
-    def checkWireguard(self,port):
-
-        result = subprocess.run(["nc", "-u", "-z", "-v", "-w5", self.host, str(port)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-
-        if result.returncode == 0:
-
-            return True
-
-        else:
-
-            return False
-
 
     def checkPing(self):
-        result = subprocess.run(["ping", "-c", "1", "-W", "2", self.host], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        result = subprocess.run(["ping", "-c", "3", "-W", "2", self.host], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         if result.returncode == 0:
-            print("Ping fonctionnel")
             return True
         else:
-            print("Non fonctionnel")
             return False
 
+    def device(self):
 
+        return False
+    
 @app.route('/', methods=["GET"])
 def main():
 
-    monitoring = Raspberry("192.168.10.3")
+    monitoring = Raspberry("172.18.153.240")
     service = {
-        "nginx": monitoring.checkServiceStatus(443),
+        "apache": monitoring.checkServiceStatus(80),
         "ssh": monitoring.checkServiceStatus(22),
         "icmp": monitoring.checkPing(),
-        "wireguard": monitoring.checkWireguard(65016),
-        "docker - Bitwarden": monitoring.checkWireguard(8080),
-        "bind": monitoring.checkWireguard(53)
     }
 
     if request.accept_mimetypes.best == 'application/json':
         return jsonify(service)
 
-    return render_template("index.html", services=service, device="Raspberry - Loulax.fr")
+    return render_template("index.html", services=service, device="Raspberry")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
